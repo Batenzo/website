20ef983e19c05b7e87f7a8b92e8876086be9cd3b
Enzo Bat <e.batungwan@alustudent.com>
2023-11-10 11:34:06 +0200

    responsive

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+
+//header scroll
+let header = document.querySelector('header');
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar')
+
+window.addEventListener('scroll', () => {
+  header.classList.toggle('shadow', window.scrollY > 0);
+});
+
+menu.onclick = () => {
+  menu.classList.toggle('bx-x');
+  navbar.classList.toggle('active');
+}
+
+window.onscroll = () => {
+  menu.classList.remove('bx-x');
+  navbar.classList.remove('active'); 
+}
+
+//Swiper
 var swiper = new Swiper(".home", {
     spaceBetween: 30,
     centeredSlides: true,
@@ -11,3 +32,19 @@ var swiper = new Swiper(".home", {
     },
     
   });
+
+  // Send email
+
+  function sendEmail(){
+    Email.send({
+      Host : "smtp.elasticemail.com",
+      Username : "username",
+      Password : "password",
+      To : 'them@website.com',
+      From : "you@isp.com",
+      Subject : "This is the subject",
+      Body : "And this is the body"
+  }).then(
+    message => alert(message)
+  );
+  }
